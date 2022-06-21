d3706830059860a578a8a867ec28960682e4b460
dynamitharry <ekstrm.j@gmail.com>
2022-06-21 20:49:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-k6a6er
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-k6a6er)
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 10
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
